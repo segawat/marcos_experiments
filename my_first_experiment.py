2d585b13023de7ecab380dfde2fce655d30367dd
Vlad Negnevitsky <vee neg nev at gee mail dot com>
2021-05-19 03:11:40 +0200

    End of Tutorial 2

--- my_first_experiment.py
+++ my_first_experiment.py
@@ -7,7 +7,22 @@ import external # imports external.py
 import experiment as ex
 
 def my_first_experiment():
-    # Write experiment here!
+    exp = ex.Experiment(lo_freq=5,
+		        rx_t=1)
+
+    event_dict = {'tx0': (np.array([50, 130, 200, 360]), np.array([0.5, 0, 0.5j, 0])),
+                  'tx1': (np.array([500, 700]), np.array([0.2, 0])),
+                  'rx0_en': (np.array([400, 800]), np.array([1, 0])),
+                  'rx1_en': (np.array([400, 800]), np.array([1, 0])) }
+    exp.add_flodict(event_dict)
+    exp.plot_sequence()
+
+    rxd, msgs = exp.run()
+    exp.close_server(only_if_sim=True)
+
+    plt.figure()
+    plt.plot(np.abs(rxd['rx1']))
+    plt.show()
 
 if __name__ == "__main__":
     my_first_experiment()

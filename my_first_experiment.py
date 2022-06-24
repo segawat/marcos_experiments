f472e6c57f357698be6537bcffb096d66ed3b2b9
unknown <segawat>
2022-06-24 15:20:47 +0200

    Add my first experiment

---
+++ my_first_experiment.py
@@ -0,0 +1,14 @@
+   import numpy as np
+   import matplotlib.pyplot as plt
+   import pdb
+   st = pdb.set_trace
+
+   import external # imports external.py
+   import experiment as ex
+
+   def my_first_experiment():
+	 exp = Experiment(lo_freq=5,
+			  rx_t=3.125)
+
+   if __name__ == "__main__":
+	 my_first_experiment()

9b2bf5f3457263d22fd9a5e4b32044ce569c260c
segawat <segawat@ethz.ch>
2022-07-01 12:58:22 +0200

    Add my first experiment

--- external.py
+++ external.py
@@ -1,3 +1,6 @@
 import sys
-sys.path.append("/home/vlad/marcos_pack/marcos_client")
-sys.path.append("/home/vlad/marcos_pack/flocra-pulseq")
+# sys.path.append("/home/vlad/marcos_pack/marcos_client")
+# sys.path.append("/home/vlad/marcos_pack/flocra-pulseq")
+
+sys.path.append("/root/marcos_pack/marcos_client")
+sys.path.append("/root/marcos_pack/flocra-pulseq")
--- my_first_experiment.py
+++ my_first_experiment.py
@@ -1,14 +1,23 @@
-   import numpy as np
-   import matplotlib.pyplot as plt
-   import pdb
-   st = pdb.set_trace
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+st = pdb.set_trace
 
-   import external # imports external.py
-   import experiment as ex
+import external # imports external.py
+import experiment as ex
 
-   def my_first_experiment():
-	 exp = Experiment(lo_freq=5,
-			  rx_t=3.125)
+def my_first_experiment():
+ exp = Experiment(lo_freq=5, rx_t=3.125)
 
-   if __name__ == "__main__":
-	 my_first_experiment()
+ tx0_times = np.array([50, 130])
+ tx0_amps = np.array([0.5, 0])
+
+ event_dict = {'tx0': (tx0_times, tx0_amps)}
+ exp.add_flodict(event_dict)
+
+ exp.add_flodict({'rx0_en': (np.array([200, 400]), np.array([1, 0]))})
+ exp.plot_sequence()
+ plt.show()
+
+if __name__ == "__main__":
+ my_first_experiment()
